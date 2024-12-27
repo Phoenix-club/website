8c3b9c908a66b0cefc1f085abed2851b4bff363b
pras2005 <prasaddesale2005@gmail.com>
2024-12-27 15:20:14 +0000

    completion and refactored api code with testing

--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,26 @@
 from rest_framework import serializers
 from rest_framework import generics
 from .models import *
+from rest_framework.exceptions import ValidationError
+
+class EventImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EventImage
+        fields = ['id','image']
 
 class EventSerializer(serializers.ModelSerializer):
+    images= EventImageSerializer(many =True,read_only =True)
     class Meta:
         model = Events
         fields="__all__"
 
+class TeamMemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =TeamMember
+        fields = ['id','name','email','phone']
+
 class RegistrationSerializer(serializers.ModelSerializer):
+    team_members = TeamMemberSerializer(many=True, required=False)
     class Meta:
         model = Registration
         fields = [
@@ -18,4 +31,19 @@ class RegistrationSerializer(serializers.ModelSerializer):
             'year',
             'event',
             'team_name',
+            'payment_screenshot',
+            'team_members',
         ]
+    def create(self , validated_data):
+        team_members_data = validated_data.pop('team_members',[])
+        registration =Registration.objects.create(**validated_data)
+
+        for member_data in team_members_data:
+            TeamMember.objects.create(registration =registration,**member_data)
+
+        return registration
+    def validate(self,data):
+        event =data['event']
+        if event.event_type =='team'and not data.get('team_name'):
+            raise serializers.ValidationError("team name is required for team registration")
+        return data
