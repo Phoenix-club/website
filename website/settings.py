b8bddf4192dbedad02af3ccbe4cc6bb304a23c1b
pras2005 <prasaddesale2005@gmail.com>
2025-01-09 18:22:14 +0000

    major fuck up cleaned up

--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#z)b_pvo2q@+qps5*fl*gbi=h2+)9c&vcjmfaomhol^!f#+r#s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vast-civil-fawn.ngrok-free.app','localhost','127.0.0.1']
 
 
 # Application definition
