4e96af6110023e3630953f926badbea90b626290
pras2005 <prasaddesale2005@gmail.com>
2025-02-25 20:45:17 +0000

    deployment.wsgi.py

--- website/wsgi.py
+++ website/wsgi.py
@@ -6,11 +6,20 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add your project directory to the system path
+sys.path.append('/home/phoenixkkw/website')  # Change to your actual project folder
 
+# Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 
+# Activate the virtual environment (named 'env')
+activate_this = '/home/phoenixkkw/env/bin/activate_this.py'
+exec(open(activate_this).read(), dict(__file__=activate_this))
+
+# Get the WSGI application
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
